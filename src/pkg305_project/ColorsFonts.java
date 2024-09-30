3522c52596cb35fb83f8a217bd533414c4569483
Leena-alnahdi <lalnahdi0008@stu.kau.edu.sa>
2024-10-01 01:09:54 +0300

    ColorsFonts class

---
+++ src/pkg305_project/ColorsFonts.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg305_project;
+
+import java.awt.Color;
+import java.awt.Font;
+
+/**
+ *
+ * @author msbbr
+ */
+public class ColorsFonts {
+    
+    public static final Font fontText = new Font("Serif",Font.BOLD, 25);
+    public static final Font fontTitle = new Font("Serif",Font.BOLD, 40);
+    public static final Color lightPurple = new Color(226,219,250);
+    public static final Color darkPurple = new Color(104,75,229);
+    
+}
