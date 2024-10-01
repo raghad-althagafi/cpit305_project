9cb9ce837b04cb9cdc43fb5958d8efce016b5af3
Raghad_Alghamdi <RALGHAMDI1254@stu.kau.edu.sa>
2024-10-01 12:04:20 +0300

    Raghad Commit

--- src/pkg305_project/ADDevent.java
+++ src/pkg305_project/ADDevent.java
@@ -37,42 +37,41 @@ public class ADDevent {
     public JFrame createFrame(String title) {
         JFrame frame = new JFrame(title);
         frame.setSize(1000, 900);
-        frame.getContentPane().setBackground(Color.GRAY);
+        frame.getContentPane().setBackground(ColorsFonts.lightPurple);
         
       //  frame.setBackground(Color.GRAY);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(null);
-        
-        JLabel e = new JLabel("Add Event");
-         e.setFont(new Font("Verdana", Font.BOLD, 30));
-        e.setForeground(Color.WHITE);
+         JLabel e = new JLabel("Add Event");
+         e.setFont(ColorsFonts.fontTitle);
+        e.setForeground(ColorsFonts.darkPurple);
         e.setBounds(340, 50, 200, 40); 
         frame.add(e);
-        
+         
         returnButton = new JButton("Return");
-        returnButton.setBounds(800, 15, 100, 30);
-        returnButton.setFont(new Font("Verdana", Font.PLAIN, 18));
+        returnButton.setBounds(800, 15, 120, 30);
+        returnButton.setFont(new Font("Verdana", Font.BOLD, 18));
         returnButton.setForeground(Color.WHITE);
-        returnButton.setBackground(Dgreen);
+        returnButton.setBackground(ColorsFonts.darkPurple);
         frame.add(returnButton);
 
         // Create and add label and text field for Event Name
         JLabel eventNameLabel = new JLabel("Event Name:");
-        eventNameLabel.setFont(new Font("Verdana", Font.PLAIN, 18));
-        eventNameLabel.setForeground(Color.WHITE);
+        eventNameLabel.setFont(ColorsFonts.fontText);
+        eventNameLabel.setForeground(ColorsFonts.darkPurple);
         eventNameLabel.setBounds(140, 150, 150, 30);
        
         frame.add(eventNameLabel);
 
         eventNameField = new JTextField();
-        eventNameField.setBounds(290, 150, 300, 30);
+        eventNameField.setBounds(290, 150, 330, 40);
         frame.add(eventNameField);
 
         // Create and add label and text field for Date
         JLabel dateLabel = new JLabel("Date:");
-        dateLabel.setFont(new Font("Verdana", Font.PLAIN, 18));
-        dateLabel.setForeground(Color.WHITE);
-        dateLabel.setBounds(140, 200, 150, 30);
+        dateLabel.setFont(ColorsFonts.fontText);
+        dateLabel.setForeground(ColorsFonts.darkPurple);
+        dateLabel.setBounds(140, 200, 150, 40);
         frame.add(dateLabel);
 
         dateField = new JTextField();
@@ -81,9 +80,9 @@ public class ADDevent {
 
         // Create and add label and text field for Time
         JLabel timeLabel = new JLabel("Time:");
-       timeLabel.setFont(new Font("Verdana", Font.PLAIN, 18));
-       timeLabel.setForeground(Color.WHITE);
-        timeLabel.setBounds(490, 200, 150, 30);
+       timeLabel.setFont(ColorsFonts.fontText);
+       timeLabel.setForeground(ColorsFonts.darkPurple);
+        timeLabel.setBounds(490, 200, 150, 40);
         frame.add(timeLabel);
 
         timeField = new JTextField();
@@ -92,43 +91,43 @@ public class ADDevent {
 
         // Create and add label and text field for Location
         JLabel locationLabel = new JLabel("Location:");
-        locationLabel.setFont(new Font("Verdana", Font.PLAIN, 18));
-        locationLabel.setForeground(Color.WHITE);
+        locationLabel.setFont(ColorsFonts.fontText);
+        locationLabel.setForeground(ColorsFonts.darkPurple);
         locationLabel.setBounds(140, 250, 150, 30);
         frame.add(locationLabel);
 
         locationField = new JTextField();
-        locationField.setBounds(290, 250, 300, 30);
+        locationField.setBounds(290, 250,  330, 40);
         frame.add(locationField);
 
         // Create and add label and text field for College
         JLabel collegeLabel = new JLabel("College:");
-        collegeLabel.setFont(new Font("Verdana", Font.PLAIN, 18));
-        collegeLabel.setForeground(Color.WHITE);
+        collegeLabel.setFont(ColorsFonts.fontText);
+        collegeLabel.setForeground(ColorsFonts.darkPurple);
         collegeLabel.setBounds(140, 300, 150, 30);
         frame.add(collegeLabel);
 
         collegeField = new JTextField();
-        collegeField.setBounds(290, 300, 300, 30);
+        collegeField.setBounds(290, 300,  330, 40);
         frame.add(collegeField);
 
         // Create and add label and text area for Details
         JLabel detailsLabel = new JLabel("Details:");
-        detailsLabel.setFont(new Font("Verdana", Font.PLAIN, 18));
-       detailsLabel.setForeground(Color.WHITE);
+        detailsLabel.setFont(ColorsFonts.fontText);
+       detailsLabel.setForeground(ColorsFonts.darkPurple);
         detailsLabel.setBounds(140, 390, 150, 30);
         frame.add(detailsLabel);
 
         detailsArea = new JTextArea();
-        detailsArea.setBounds(290, 400, 300, 100);
+        detailsArea.setBounds(290, 400, 330, 130);
         frame.add(detailsArea);
 
         // Create and add "Add Event" button
         addButton = new JButton("ADD");
-        addButton.setBounds(340, 600, 200, 40);
-        addButton.setFont(new Font("Verdana", Font.PLAIN, 18));
+        addButton.setBounds(290, 670, 350, 40);
+        addButton.setFont(ColorsFonts.fontText);
         addButton.setForeground(Color.WHITE);
-        addButton.setBackground(Dgreen);
+        addButton.setBackground(ColorsFonts.darkPurple);
         frame.add(addButton);
         addButton.addActionListener(new ActionListener() {
             @Override
